7a99e9349fbdd195dd09e5407ba0483f90fb1d94
Isaac <asabaisaac1@gmail.com>
2024-06-18 11:15:03 +0200

    added all

--- src/main/java/com/example/bloggheaven/config/SecurityConfig.java
+++ src/main/java/com/example/bloggheaven/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
@@ -49,6 +50,8 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+
 }
 
 
